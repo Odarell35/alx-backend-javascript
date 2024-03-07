a96f9c87cb11bb5e9a7851b4a71141400c98c802
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-03-07 21:01:59 +0200

    task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from "./3-currency.js";
+
+export default class Pricing {
+	constructor(amount, currency) {
+		this._amount = amount;
+		this._currency = currency;
+	}
+
+	get amount() {
+		return this._amount;
+	}
+
+	set amount(newamount) {
+		this._amount = newamount;
+	}
+
+	get currency() {
+		return this._currency;
+	}
+
+	set currency(newcurr) {
+		this._currency = newcurr;
+	}
+
+	displayFullPrice() {
+		return `${this._amount} ${this.currency.name} (${this.currency.code})`;
+	}
+
+	static convertPrice(amount, conversionRate) {
+		return `${amount} * ${conversionRate}`
+	}
+}
