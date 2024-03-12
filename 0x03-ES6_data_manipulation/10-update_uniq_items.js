7577d34af478d527878c8ed7a5548f447d81f5d4
Odarell35 <leratomawash0@gmail.com>
2024-03-12 14:33:11 +0200

    eslint

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,13 @@
 export default function updateUniqueItems(inputMap) {
-    if (!(inputMap instanceof Map)) {
-        throw new Error('Cannot process');
-    }
+  if (!(inputMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
 
-    const updatedMap = new Map();
-    
-    for (const [i, q] of inputMap) {
-        updatedMap.set(i, q === 1 ? 100 : q);
-    }
+  const updatedMap = new Map();
 
-    return updatedMap;
+  for (const [i, q] of inputMap) {
+    updatedMap.set(i, q === 1 ? 100 : q);
+  }
+
+  return updatedMap;
 }
