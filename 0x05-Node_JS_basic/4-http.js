ce5df60a102f8b67f5f895ce4ed1e61b2f534528
Odarell35 <leratomawasha0@gmail.com>
2024-05-02 11:12:44 +0000

    http setup

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,14 +1,13 @@
 // create a small HTTP server using the http module:
 const http = require('http');
+const hostname = '127.0.0.1';
 const app = http.createServer((req, res) => {
+    res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    
-    res.end('Hello Holberton School!\n');
+    res.end('Hello Holberton School!');
 });
 
 const PORT = 1245;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(PORT, hostname);
 
 module.exports = app;
