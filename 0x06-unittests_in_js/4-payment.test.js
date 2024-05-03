3e4a8856172178a9bb88154fec2cb814d58cf572
Odarell35 <leratomawasha0@gmail.com>
2024-05-03 10:29:59 +0000

    Update

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,17 +4,18 @@ const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 
-describe('Spies', function () {
-  it('has the same math', () => {
-    const spyUtils = sinon.spy(Utils, 'calculateNumber');
+describe('Stubbing', function () {
+  it('should stub calculateNumber to return 10', () => {
+    const stubCalculateNumber = sinon.stub(Utils, 'calculateNumber').returns(10);
     const spyConsole = sinon.spy(console, 'log');
 
     sendPaymentRequestToApi(100, 20);
 
-    expect(spyUtils.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-    expect(spyConsole.calledOnceWithExactly('The total is: 120')).to.be.true;
+    expect(stubCalculateNumber.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(spyConsole.calledOnceWithExactly('The total is: 10')).to.be.true;
 
-    spyUtils.restore();
+    stubCalculateNumber.restore();
     spyConsole.restore();
   });
 });
+
